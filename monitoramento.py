90711dd4aa3b3be98f10ba47f3c4744e9dc5bb0c
somaicaro <131361630+somaicaro@users.noreply.github.com>
2023-05-09 14:01:12 -0300

    Update monitoramento.py
    Adicionadas linhas para armazenar as informações no banco de dados.

--- monitoramento.py
+++ monitoramento.py
@@ -1,14 +1,26 @@
-import rotina, fluxo, infra, conn, envia
+import conn
+from datetime import datetime
 
-msg1 = rotina.monitora()
-msg2 = fluxo.monitora()
-msg3 = infra.monitora()
+cur = conn.conexao_bd.cursor()
+cur.execute("select mes_monitorado from monitoramento")
+mes_bd = cur.fetchall()[-1][0]
 
-conn.conexao_bd.close()
+mes_atual = datetime.now().month
 
-with open('monitorado.txt', 'w', encoding="UTF-8") as arquivo:   
-    arquivo.write("""O monitoramento mensal do SOMA Tarifador foi concluído. 
-Seguem as informações para seu conhecimento:\n""")
-    arquivo.write(f"{msg1}\n{msg2}\n{msg3}")
+if (mes_bd == mes_atual):
+    pass
+else:
+    import rotina, fluxo, infra, envia
 
-envia.email()
+    msg1 = rotina.monitora()
+    msg2 = fluxo.monitora()
+    msg3 = infra.monitora()
+
+    conn.conexao_bd.close()
+
+    with open('monitorado.txt', 'w', encoding="UTF-8") as arquivo:   
+        arquivo.write("""O monitoramento mensal do SOMA Tarifador foi concluído. 
+    Seguem as informações para seu conhecimento:\n""")
+        arquivo.write(f"{msg1}\n{msg2}\n{msg3}")
+
+    envia.email()
