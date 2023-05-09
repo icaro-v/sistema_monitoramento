85d5fdb93b547fd52a14e46c44d652e4baefd844
somaicaro <131361630+somaicaro@users.noreply.github.com>
2023-05-09 14:00:16 -0300

    Update infra.py
    Adicionadas linhas para armazenar as informações no banco de dados.

--- infra.py
+++ infra.py
@@ -1,5 +1,6 @@
 def monitora():
-    import psutil
+    import psutil, conn
+    from datetime import datetime
 
     # obter informações de uso de disco
     disk_usage = psutil.disk_usage('/')
@@ -9,6 +10,13 @@ def monitora():
     livre = (disk_usage.free / (2**30))
     livre = f'{livre:.2f}'
 
+    mes_monitorado = datetime.now().month 
+
+    cur = conn.conexao_bd.cursor()
+    cur.execute(f"""update monitoramento set espaco_gb = '{livre}'
+    WHERE mes_monitorado = {mes_monitorado}""")
+    conn.conexao_bd.commit()
+    
     return f"""INFRA
-    Espaço em disco (HD): {livre} GB\n
+    Espaço em disco (HD): {livre} GB
     """
